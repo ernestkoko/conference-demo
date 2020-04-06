81655709bfe951f8015a29b97eee0a4e66b2f9a0
ernestkoko <ernesteferetin@yahoo.com>
2020-04-06 15:31:40 +0100

    Added @RestController and @RequestMapping to tell the url requests to be sent to the specified controller

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -1,4 +1,9 @@
 package com.pluralsight.conferencedemo.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController  //this makes this class the controller. All requests to this below url will be sent to this controller
+@RequestMapping("api/v1/sessions") //this is what the mapping url will look like
 public class SessionsController {
 }
