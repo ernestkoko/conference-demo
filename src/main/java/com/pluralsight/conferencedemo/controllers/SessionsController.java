03ee5694f8b65255434a8d34bcd8645e99d63f73
ernestkoko <ernesteferetin@yahoo.com>
2020-04-06 15:53:52 +0100

    Completed the Controllers and autowired them to the Repositiories

--- src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SessionsController.java
@@ -1,9 +1,33 @@
 package com.pluralsight.conferencedemo.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pluralsight.conferencedemo.models.Session;
+import com.pluralsight.conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController  //this makes this class the controller. All requests to this below url will be sent to this controller
-@RequestMapping("api/v1/sessions") //this is what the mapping url will look like
+@RequestMapping("/api/v1/sessions") //this is what the mapping url will look like
 public class SessionsController {
+
+    //@Autowired autowires this controller
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    public List<Session> list(){
+        return sessionRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Session get(@PathVariable Long id){
+        return sessionRepository.getOne(id);
+    }
+//
+//    @PostMapping
+//    public Session create(@RequestBody final Session session){
+//        return sessionRepository.saveAndFlush(session);
+//    }
 }
