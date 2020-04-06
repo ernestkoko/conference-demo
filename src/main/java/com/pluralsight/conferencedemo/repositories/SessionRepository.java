34b9bc26dd2ff90e8d2032ff6f13b95fc386b9da
ernestkoko <ernesteferetin@yahoo.com>
2020-04-06 15:27:42 +0100

    Created the Repositories as an interfaces and the SessionsController

--- src/main/java/com/pluralsight/conferencedemo/repositories/SessionRepository.java
+++ src/main/java/com/pluralsight/conferencedemo/repositories/SessionRepository.java
@@ -1,4 +1,7 @@
 package com.pluralsight.conferencedemo.repositories;
 
-public interface SessionRepository {
+import com.pluralsight.conferencedemo.models.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
 }
