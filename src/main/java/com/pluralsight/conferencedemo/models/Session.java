73ed6e80c925491d6f1cf6818bc3dedd3dfd2f53
ernestkoko <ernesteferetin@yahoo.com>
2020-04-06 15:00:25 +0100

    joined the two tables to form session_speakers tables

--- src/main/java/com/pluralsight/conferencedemo/models/Session.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Session.java
@@ -1,9 +1,7 @@
 package com.pluralsight.conferencedemo.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity(name = "sessions")
 public class Session {
@@ -16,8 +14,26 @@ public class Session {
     private String session_description;
     private Integer session_length;
 
+    //ManyToMany is jpa annotation for connecting the two tables
+    //JoinTable joins the two tables
+    @ManyToMany
+    @JoinTable(
+            name = "session_speakers",
+            joinColumns = @JoinColumn(name = "session_id"),
+            inverseJoinColumns = @JoinColumn(name = "speaker_id")
+    )
+    private List<Speaker> speakers; //list of speakers
+
     //constructor
     public Session(){}
+
+    public List<Speaker> getSpeakers() {
+        return speakers;
+    }
+
+    public void setSpeakers(List<Speaker> speakers) {
+        this.speakers = speakers;
+    }
 
     public Long getSession_id() {
         return session_id;
